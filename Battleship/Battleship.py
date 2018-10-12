0d85f8b1e28446da86e5c9633142c1ddf577e027
davidhnguyen2000 <davidhnguyen2000@gmail.com>
2018-10-12 11:23:54 +0100

    Fixed player boat placement and put in preliminary AI code for guessing where battleships are for the computer

--- Battleship/Battleship.py
+++ Battleship/Battleship.py
@@ -34,7 +34,7 @@ def placeBoat(startX, startY, direction, battleshipSize, gameBoardSize, gameBoar
                 for xCoor in range(battleshipSize):
                     newXCoor = startX + direction2 * xCoor
                     gameBoard[newXCoor][startY] = BATTLESHIP_CHAR
-                    print('placed something')
+                    # print('placed something')
         elif direction == 1 or direction == 3:
             if direction == 1:
                 direction2 = -1
@@ -52,7 +52,7 @@ def placeBoat(startX, startY, direction, battleshipSize, gameBoardSize, gameBoar
                 for yCoor in range(battleshipSize):
                     newYCoor = startY + direction2 * yCoor
                     gameBoard[startX][newYCoor] = BATTLESHIP_CHAR
-                    print('placed something')
+                    # print('placed something')
         return True
     else:
         return False
@@ -70,7 +70,7 @@ def placeBoatComputer(battleshipSize, gameBoardSize):
 def placeBoatPlayer(battleshipSize, gameBoardSize):
     startX = int(input("X coordinate of start:"))
     startY = int(input("Y coordinate of start:"))
-    direction = int(input("direction\n 1: up\n 2: down\n 3: right\n 4: left\n choice:"))
+    direction = int(input("direction\n 0: left\n 1: up\n 2: right\n 3: down\n choice:"))
     if not placeBoat(startX, startY, direction, battleshipSize, gameBoardSize, playerGameBoard):
         print("Invalid placement")
         placeBoatPlayer(battleshipSize, gameBoardSize)
@@ -99,21 +99,49 @@ def displayGameboard(board):
         print()
     return
 
-def guess(totalHits):
+def guessPlayer(totalHitsPlayer):
     guessX = int(input("x coordinate: "))
     guessY = int(input("y coordinate: "))
     if computerGameBoard[guessX][guessY] == BATTLESHIP_CHAR:
         computerGameBoard[guessX][guessY] = "X"
         print("HIT")
-        totalHits = totalHits + 1
+        totalHitsPlayer = totalHitsPlayer + 1
     else:
         computerGameBoard[guessX][guessY] = "o"
         print("SPLASH")
 
+def guessComputer(totalHitsComputer, lastGuessX, lastGuessY, hitLastTurn): # preliminary AI code --> look at using binary tree for decision making
+    if not hitLastTurn:
+        guessX = random.randint(0, 9)
+        guessY = random.randint(0, 9)
+    else:
+        direction = random.randint(0, 3)
+        if direction == 0 and lastGuessX + 1 < 10:
+            guessX = lastGuessX + 1
+        elif direction == 1 and lastGuessX - 1 >= 0:
+            guessX = lastGuessX - 1
+        elif direction == 2 and lastGuessY + 1 < 10:
+            guessY = lastGuessY + 1
+        elif direction == 3 and lastGuessY -1 >= 0:
+            guessY = lastGuessY -1
+
+
+    if playerGameBoard[guessX][guessY] == BATTLESHIP_CHAR:
+        playerGameBoard[guessX][guessY] = "X"
+        print("HIT")
+        totalHitsComputer = totalHitsComputer + 1
+        lastGuessX = guessX
+        lastGuessY = guessY
+        hitLastTurn = True
+    else:
+        playerGameBoard[guessX][guessY] = "o"
+        print("SPLASH")
+        hitLastTurn = False
+
+
 def createPlayerGameboard(gameBoardSize):
     "This creates the game board of the player through user input"
     playerBoard = makeGameboard(gameBoardSize)
-
     return playerBoard
 
 
@@ -122,22 +150,37 @@ BATTLESHIP_1_SIZE = 5
 BATTLESHIP_2_SIZE = 4
 BATTLESHIP_3_SIZE = 3
 BATTLESHIP_4_SIZE = 2
-totalHits = 0
+playerHits = 0
+computerHits = 0
+lastGuessX = 0
+lastGuessY = 0
+hitLastTurn = False
 
 
 computerGameBoard = makeGameboard(GAME_BOARD_SIZE)
+
 placeBoatComputer(BATTLESHIP_1_SIZE, GAME_BOARD_SIZE)
 placeBoatComputer(BATTLESHIP_2_SIZE, GAME_BOARD_SIZE)
 placeBoatComputer(BATTLESHIP_3_SIZE, GAME_BOARD_SIZE)
 placeBoatComputer(BATTLESHIP_4_SIZE, GAME_BOARD_SIZE)
-playerGameBoard = createPlayerGameboard(GAME_BOARD_SIZE)
 
 displayGameboardWithBoats(computerGameBoard)
+
+playerGameBoard = createPlayerGameboard(GAME_BOARD_SIZE)
 displayGameboard(playerGameBoard)
+
 placeBoatPlayer(BATTLESHIP_1_SIZE, GAME_BOARD_SIZE)
 displayGameboardWithBoats(playerGameBoard)
-# while totalHits < 13:
-#     displayGameboard(computerGameBoard)
-#     guess(totalHits)
-#     print()
-# print("You win!")
+placeBoatPlayer(BATTLESHIP_2_SIZE, GAME_BOARD_SIZE)
+displayGameboardWithBoats(playerGameBoard)
+placeBoatPlayer(BATTLESHIP_3_SIZE, GAME_BOARD_SIZE)
+displayGameboardWithBoats(playerGameBoard)
+placeBoatPlayer(BATTLESHIP_4_SIZE, GAME_BOARD_SIZE)
+displayGameboardWithBoats(playerGameBoard)
+
+
+while playerHits < 13 and computerHits < 13:
+    displayGameboard(computerGameBoard)
+    guess(totalHits)
+    print()
+print("You win!")
